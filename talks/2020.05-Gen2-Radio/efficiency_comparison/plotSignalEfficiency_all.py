d77b219bb1c9962987307f291af978742b6911ee
clark2668 <baclark@msu.edu>
2020-05-05 11:43:40 -0500

    fix axis label on energy plot

--- talks/2020.05-Gen2-Radio/efficiency_comparison/plotSignalEfficiency_all.py
+++ talks/2020.05-Gen2-Radio/efficiency_comparison/plotSignalEfficiency_all.py
@@ -10,7 +10,7 @@ ax = plt.subplot(1,1,1)
 data_a2_snr = np.genfromtxt("A2_signalEfficiencyVsChannelInWindowSNR_E-2.13.csv",delimiter=',',skip_header=0,names=['snr','eff'])
 a2_snr = data_a2_snr['snr']
 a2_snr_eff = data_a2_snr['eff']
-ax.plot(a2_snr, a2_snr_eff, '-', color='blue', label='A2', linewidth=5)
+ax.plot(a2_snr, a2_snr_eff, '-', color='blue', label='A2, 4 years', linewidth=5)
 
 data_tb_snr = np.genfromtxt("testbed_eff_vs_snr.csv",delimiter=',',skip_header=1,names=['snr','eff'])
 tb_snr = data_tb_snr['snr']
@@ -42,7 +42,7 @@ a2_energy = data_a2_energy['energy']
 a2_energy_eff = data_a2_energy['eff']
 a2_energy = np.log10(a2_energy)
 a2_energy+=9
-ax2.plot(a2_energy, a2_energy_eff, '-', color='blue', label='A2', linewidth=5)
+ax2.plot(a2_energy, a2_energy_eff, '-', color='blue', label='A2, 4 years', linewidth=5)
 
 data_tb_energy = np.genfromtxt("testbed_eff_vs_energy.csv",delimiter=',',skip_header=1,names=['energy','eff'])
 tb_energy = data_tb_energy['energy']
@@ -52,7 +52,7 @@ ax2.plot(tb_energy, tb_energy_eff, '--', color='grey', label='Prototype', linewi
 
 ax2.set_ylim(0,1.1)
 ax2.set_xlim(15.5,21.5)
-ax2.set_xlabel('SNR', fontsize=30)
+ax2.set_xlabel(r'Energy [log10(eV)]', fontsize=30)
 ax2.set_ylabel('Signal Efficiency', fontsize=30)
 # ax.set_title('A2 Efficiency vs SNR', fontsize=30)
 ax2.grid(which='both',axis='y')
