2a524af55c88a8580104536a0ee973e919dd7c1c
Brian Clark <baclark@msu.edu>
2021-04-08 17:46:07 -0400

    add a livetime vs time plot for 2021 aps april meeting

---
+++ talks/2021.04-APS-April/plot_livetime_vs_time.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('livetime_vs_time.csv',delimiter=',',skip_header=1,
+	names=['Year','Month','Date','Sum'])
+date = data['Date']
+livetime = data['Sum']
+start_year=2012
+stop_year=2021
+
+fig = plt.figure(figsize=(7,5 ))
+ax=fig.add_subplot(1,1,1)
+ax.plot(date,livetime,'o')
+ax.tick_params(labelsize=14)
+ax.grid(True,'major','x',linestyle='--');
+ax.set_xlabel('Year',size=14)
+ax.set_ylabel(r'Total Accumulated Livetime [years]', size=14)
+plt.tight_layout()
+fig.savefig('livetime_vs_time.png',edgecolor='none',bbox_inches="tight",dpi=300, w_pad=1.5)
